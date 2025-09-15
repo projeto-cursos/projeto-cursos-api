ea80471855a328c5bc09fb345786c1df16238133
unknown <rhuanalbuquerque6065@digitalcollege.internal>
2025-09-15 17:56:27 -0300

    rota delet

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ app.post("/api/cursos", async (req, res) => {
 
 
 //Rhuan......................................................................
-
+app.delete('/api/cursos/:id', async (req, res) => {
+  const { id } = req.params;
+  const { error } = await supabase.from('cursos').delete().eq('id', id);
+  if (error) return res.status(400).json({ error: error.message });
+  res.json({ message: 'Curso deletado com sucesso' });
+});
 
 //Mauricio...................................................................
 app.put('/api/cursos/:id', async (req, res) => {
